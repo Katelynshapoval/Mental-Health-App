81b54e18e7cff9072daf1245051e3319e9004cfc
Katelyn Shapoval <katelyn.shapoval@gmail.com>
2024-10-31 13:57:50 +0100

    navbars

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import "./App.css";
+import { IoHome } from "react-icons/io5";
+import { IoChatboxEllipses } from "react-icons/io5";
+import { FaQuestionCircle } from "react-icons/fa";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { MdTipsAndUpdates } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <div className="navigation">
+        <div className="sidebar">
+          <img src="images/logo.png" alt="logo" id="logo" />
+          <ul class="menuOptions">
+            <li>
+              <IoHome />
+              Home
+            </li>
+            <li>
+              <FaQuestionCircle />
+              About us
+            </li>
+            <li>
+              <IoChatboxEllipses />
+              Chat
+            </li>
+            <li>
+              <MdTipsAndUpdates />
+              Tips
+            </li>
+          </ul>
+        </div>
+        <div className="topBar">
+          <p>
+            Take care <IoIosHeartEmpty />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
